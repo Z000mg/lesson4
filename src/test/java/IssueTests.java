8728a5b5fc8bcd5dd74dc58f80af291044774be3
z000mg <zooomg2015@yandex.ru>
2020-11-25 18:06:11 +0300

    1st

---
+++ src/test/java/IssueTests.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class IssueTests {
+
+    private static String Repo = "Z000mg/lesson4";
+    private static String User = "Z000mg";
+    private static int IssueNumber = 1;
+
+    @Test
+    public void searchForIssue () {
+        open("https://github.com/");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(Repo);
+        $(".header-search-input").submit();
+        $(By.linkText(Repo)).click();
+        $(withText("Issues")).click();
+        $(withText("#" + IssueNumber)).should(Condition.exist);
+    }
+}
+
